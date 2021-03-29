d972153e903a9235275a0942312d35356f06bb64
SafitriNur <nursafitri6692@gmail.com>
2021-03-29 08:21:52 +0700

    Coba login

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -15,14 +15,20 @@
                                 <div class="text-center">
                                     <h1 class="h4 text-gray-900 mb-4">Login Page</h1>
                                 </div>
-                                <form class="user">
+
+                                <?= $this->session->flashdata('message'); ?>
+
+                                <form class="user" method="post" action="<?= base_url('auth'); ?>">
                                     <div class="form-group">
                                         <input type="text" class="form-control form-control-user" id="email"
-                                            name="email" placeholder="Enter Email Address...">
+                                            name="email" placeholder="Enter Email Address..."
+                                            value="<?= set_value('email'); ?>">
+                                        <?= form_error('email', '<small class="text-danger pl-3">', '</small>'); ?>
                                     </div>
                                     <div class="form-group">
                                         <input type="password" class="form-control form-control-user" id="password"
                                             name="password" placeholder="Password">
+                                        <?= form_error('password', '<small class="text-danger pl-3">', '</small>'); ?>
                                     </div>
                                     <!-- <div class="form-group">
                                             <div class="custom-control custom-checkbox small">
@@ -31,10 +37,10 @@
                                                     Me</label>
                                             </div>
                                         </div> -->
-                                    <button type="submit" class="btn btn-primary btn-user btn-block">
+                                    <button type="submit" class="btn btn-success btn-user btn-block">
                                         Login
                                     </button>
-                                    <button type="submit" class="btn btn-warning btn-user btn-block">
+                                    <button type="submit" class="btn btn-primary btn-user btn-block">
                                         Login Sebagai Siswa
                                     </button>
 
@@ -43,9 +49,9 @@
                                 <div class="text-center">
                                     <a class="small" href="forgot-password.html">Forgot Password?</a>
                                 </div>
-                                <div class="text-center">
+                                <!-- <div class="text-center">
                                     <a class="small" href="register.html">Create an Account!</a>
-                                </div>
+                                </div> -->
                             </div>
                         </div>
                     </div>
