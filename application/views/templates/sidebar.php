7b5ba0982463f8899d3b91bc82e82fd23f081fe1
kiki <rahmarizki37@gmail.com>
2021-04-20 08:33:05 +0700

    fixing sidebar admin

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -4,9 +4,9 @@
             <!-- Sidebar - Brand -->
             <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
                 <div class="sidebar-brand-icon">
-                    <i class="fas fa-users"></i>
+                    <img class="img-profile" height="50" width="50" src="<?= base_url('assets/img/admin2.png') ?>">
                 </div>
-                <div class="sidebar-brand-text mx-3">SI SMP 2 MOJO</div>
+                <div class="sidebar-brand-text mx-3">Admin</div>
             </a>
 
             <!-- Divider -->
--- application/views/templates/topbar.php
+++ application/views/templates/topbar.php
@@ -4,7 +4,7 @@
     <!-- Main Content -->
     <div id="content">
         <!-- Topbar -->
-        <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+        <nav class="navbar navbar-expand navbar-light bg-admin topbar mb-4 static-top shadow">
 
             <!-- Sidebar Toggle (Topbar) -->
             <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
@@ -76,8 +76,8 @@
                 <!-- Nav Item - User Information -->
                 <li class="nav-item dropdown no-arrow">
                     <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?= $guru['nama']; ?></span>
-                        <img class="img-profile rounded-circle" src="https://image.flaticon.com/icons/png/512/104/104784.png">
+                        <span class="mr-2 d-none d-lg-inline text-white-600 large"><?= $guru['nama']; ?></span>
+                        <img class="img-profile rounded-circle" src="<?= base_url('assets/img/admin.png') ?>">
                     </a>
                     <!-- Dropdown - User Information -->
                     <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
