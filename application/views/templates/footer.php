1360bf116d7a0587acf8c42db5adc378e335571f
kiki <rahmarizki37@gmail.com>
2021-05-23 10:25:02 +0700

    delete sweetalert

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -34,9 +34,7 @@
 <script src="<?= base_url('assets/'); ?>js/sb-admin-2.min.js"></script>
 
 <!-- Sweet Alert -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.33.1/sweetalert2.min.js"></script>
+
 
 </body>
 
