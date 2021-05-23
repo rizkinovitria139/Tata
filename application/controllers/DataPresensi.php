b0291bc4a2b0b5aaa3030dfacc3c19f8ed8dc4cb
SafitriNur <nursafitri6692@gmail.com>
2021-05-23 15:13:11 +0700

    coba presensi

--- application/controllers/DataPresensi.php
+++ application/controllers/DataPresensi.php
@@ -4,6 +4,14 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class DataPresensi extends CI_Controller
 {
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('Admin_model');
+        $this->load->library('form_validation');
+    }
+
     public function index()
     {
         $data['title'] = 'Data Presensi Siswa';
@@ -35,6 +43,40 @@ class DataPresensi extends CI_Controller
         $this->load->view('templates/topbar', $data);
         $this->load->view('admin/dataPresensi', $data);
         $this->load->view('templates/footer');
+    }
+    
+    public function get_presensi()
+    {
+        $data['title'] = 'Daftar Kelas';
+        $data['Guru_Mapel'] = $this->db->get_where('guru', ['username' => $this->session->userdata('username')])->row_array();
+
+        $this->session->set_userdata($data);
+
+
+        $this->load->model('kelas_model', 'kelas');
+        $data['kelas'] = $this->kelas->get_kelas();
+        $this->load->model('Admin_model', 'guru');
+        $data['guru'] = $this->guru->getAll();
+        $this->load->model('Tahun_model', 'tahun_akademik');
+        $data['tahun_akademik'] = $this->tahun_akademik->getAll();
+
+        $this->session->set_userdata($data);
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar');
+        $this->load->view('templates/topbar', $data);
+        $this->load->view('guruMapel/kelas', $data);
+        $this->load->view('templates/footer');
+    }
+
+    public function edit_presensi($id)
+    {
+        $this->db->update('presensi', ['bulan' => $this->input->post('bulan')], ['id_presensi' => $id]);
+        $this->db->update('presensi', ['hadir' => $this->input->post('hadir')], ['id_presensi' => $id]);
+        $this->db->update('presensi', ['sakit' => $this->input->post('sakit')], ['id_presensi' => $id]);
+        $this->db->update('presensi', ['alpha' => $this->input->post('alpha')], ['id_presensi' => $id]);
+        
+        $this->session->set_flashdata('kelas_message', '<div class="alert alert-success" role="alert">Kelas berhasil diubah!</div>');
+        redirect('DataPresensi/get_presensi', 'refresh');
     }
     
 }
