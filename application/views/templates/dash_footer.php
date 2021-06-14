bf06df6735df7d5bf418ac5ec69176f96610cdba
kiki <rahmarizki37@gmail.com>
2021-06-15 05:31:19 +0700

    footer

--- application/views/templates/dash_footer.php
+++ application/views/templates/dash_footer.php
@@ -1,8 +1,8 @@
 <!-- ======= Footer ======= -->
 <footer id="footer">
     <div class="container">
-        <h3>Selecao</h3>
-        <p>Et aut eum quis fuga eos sunt ipsa nihil. Labore corporis magni eligendi fuga maxime saepe commodi placeat.</p>
+        <h3>SMP Negeri 2 Mojo</h3>
+        <p>Sekolah Negeri dengan pelayanan terbaik</p>
         <div class="social-links">
             <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
             <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
@@ -11,14 +11,14 @@
             <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
         </div>
         <div class="copyright">
-            &copy; Copyright <strong><span>Selecao</span></strong>. All Rights Reserved
+            &copy; Copyright <strong><span>SMP Negeri 2 MOjo</span></strong>. All Rights Reserved
         </div>
         <div class="credits">
             <!-- All the links in the footer should remain intact. -->
             <!-- You can delete the links only if you purchased the pro version. -->
             <!-- Licensing information: https://bootstrapmade.com/license/ -->
             <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/selecao-bootstrap-template/ -->
-            Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+            <!-- Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a> -->
         </div>
     </div>
 </footer><!-- End Footer -->
